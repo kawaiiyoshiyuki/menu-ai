e35c362cd6398c38d7bb765bbe33118ec5b5b253
habr <jonas.habr@t-mobile.cz>
2021-09-05 07:48:34 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    alanBtn({
+      key:
+        "e3a32cd64395a643bd3a03cee62943562e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "getMenu") {
+          setProducts(commandData.data)
+          console.log(commandData)
+        }
+        else if (commandData.command === "addToCart") {
+          setCart((currentCart) => [...currentCart, commandData.data])
+        }
+      },
+    })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Menu</h1>
+      {products.map((product) => (
+        <div key={product.name}>
+          {product.name} - {`$${product.price}`} - {product.category}
+        </div>
+      ))}
+      <h2>Cart:</h2>
+      {cart.map((cartItem) => (
+        <div key={cartItem.name} className="bt-2">
+          {cartItem.name} - {`$${cartItem.price}`} - {cartItem.category}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
